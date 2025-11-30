ac61f9f0ef75b708432ac2cea2ad877b686f8106
David Pillco <contact.davidpill@gmail.com>
2025-11-30 13:38:46 +0100

    add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidpillco.github.io',
+  base: 'mi-repo',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
